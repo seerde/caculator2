37f7b5b8580d699e5719843f6a791d328304de4f
seerde <mr.abod7oom@gmail.com>
2019-05-09 17:01:13 +0900

    .

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
 #test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
-CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
-echo $CALCULATOR_PORT
-test $(curl --silent 127.0.0.1:$CALCULATOR_PORT/sum?a=10\&b=20) -eq 30
+test $(curl --silent 127.0.0.1:8090/sum?a=10\&b=20) -eq 30
