85596be65f80079ef17e47198e345e500e5cca95
XYZ <xyz@abc.com>
2019-05-09 14:34:45 +0900

    modift acceptance_sh for Docker-compose

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
-test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
+#test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
+#test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
+CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
+test $(curl --silent -X GET localhost:$CALCULATOR_PORT/sum?a=10\&b=20) -eq 30
